e8810b6fe0ae9f11f29ed397526a64717047c2b6
Nick Valdez <nicholastvaldez@gmail.com>
2023-10-03 09:10:33 -0500

    install react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
 import App from "./App"
 import theme from "./theme"
